8f6fc6305e013aca2b038e184ee066b98defec3c
FasihaKhanamDalazak <fasihakhan.dalazak@gmail.com>
2025-03-10 21:33:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -58,6 +58,6 @@ btn.addEventListener("click", (evt) => {
   else{
     errMsg.style.display= "block";
     input.value="";
-    msg.innerText="Enter amount to convert.";
+     msg.innerText="Enter amount to see exchange rate.";
   }
 });
